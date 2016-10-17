af473b1ffd27ba4b5b3ed7b747cc1e1ae6ee54ed
Jignesh Darji <jdarji@JDARJI-MBPR15.local>
2016-10-17 14:05:40 -0400

    listening to entire message on mentions and direct message

--- slackbot/bot.js
+++ slackbot/bot.js
@@ -19,29 +19,30 @@ controller.spawn({
 
 // give the bot something to listen for.
 //controller.hears('string or regex',['direct_message','direct_mention','mention'],function(bot,message) {
-controller.hears('weather',['mention', 'direct_mention'], function(bot,message) {
-    console.log('someone mentioned me! Yay!');
-    getWeather(function(w) {
-        bot.reply(message, w);
-    });
+controller.hears('(.*)',['direct_mention', 'direct_message', 'weather'], function(bot,message) {
+  console.log('someone mentioned me! Yay! message: ' + message.match[0]);
+  getWeather(function(w) {
+    bot.reply(message, w);
+  });
 });
+
 
 // example for calling weather api
 function getWeather(callback)
 {
-    var latitude = "48.208579"
-    var longitude = "16.374124"
-    var message = "Hey, @git_bot The weather is "
-    forecast.get(latitude, longitude, function (err, res, data) 
-    {
-        console.log('message generated: ' + message);
-        if (err) throw err;
-        //console.log('res: ' + JSON.stringify(res));
-        //console.log('data: ' + JSON.stringify(data));
-        w = data.currently.summary + " and feels like " +
-        data.currently.apparentTemperature;
-        message = message + w;
-        console.log('message generated: ' + message);
-        callback(message);
-    });
+  var latitude = "48.208579"
+  var longitude = "16.374124"
+  var message = "Hey, @git_bot The weather is "
+  forecast.get(latitude, longitude, function (err, res, data)
+  {
+    console.log('message generated: ' + message);
+    if (err) throw err;
+    //console.log('res: ' + JSON.stringify(res));
+    //console.log('data: ' + JSON.stringify(data));
+    w = data.currently.summary + " and feels like " +
+    data.currently.apparentTemperature;
+    message = message + w;
+    console.log('message generated: ' + message);
+    callback(message);
+  });
 }
