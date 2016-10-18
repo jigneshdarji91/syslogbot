18b74cf19b0cb578a5664c1befd8f0215b640d9d
Prathamesh <pghanek@ncsu.edu>
2016-10-18 16:37:40 -0400

    removing the stray lines that unknowingly appeared in the earlier commit

--- slackbot/queryGenerator.js
+++ slackbot/queryGenerator.js
@@ -1,57 +1,57 @@
 /* * * * * * * * * * * * * * * * * * * *
-Author: Prathamesh Ghanekar(pghanek)
-Student ID: 200110766
+   Author: Prathamesh Ghanekar(pghanek)
+   Student ID: 200110766
  * * * * * * * * * * * * * * * * * * * */
 
 /*this function generates a mySQL query from message object*/
 function objectToQuery(object) {
-  console.log("object: " + JSON.stringify(object));
-  var table_name = "logtable";// subject to change
-  var first_entry = 1;
-  var count_flag = 0;
-  var limit;
- 
-  var query = "SELECT * FROM " + table_name + " WHERE ";
-  for (var key in object) {
+    console.log("object: " + JSON.stringify(object));
+    var table_name = "logtable";// subject to change
+    var first_entry = 1;
+    var count_flag = 0;
+    var limit;
 
-  	if (object.hasOwnProperty(key)){
-  		if(key == 'type')
-  			continue;
+    var query = "SELECT * FROM " + table_name + " WHERE ";
+    for (var key in object) {
 
-  		if(key == 'count'){
-  			count_flag = 1;
-  			limit = object['count'];
-  			continue;
-  		}
+        if (object.hasOwnProperty(key)){
+            if(key == 'type')
+                continue;
 
-  		if(!first_entry){
-  		query = query + " AND ";
-  		}
-		first_entry = 0;  	
+            if(key == 'count'){
+                count_flag = 1;
+                limit = object['count'];
+                continue;
+            }
 
-    	//console.log(key + " -> " + object[key]);
-    	//if (key != 'count'){
-	    	query = query + key + " LIKE ";
-	    
-	    	for(i = 0; i < object[key].length; i++){    		
-	    		query = query + object[key][i];
-	    		if(i != object[key].length - 1){
-	    			query = query + " OR ";
-	    		}
-	    	}
-  		//}
-  		
-   	}
-}
+            if(!first_entry){
+                query = query + " AND ";
+            }
+            first_entry = 0;  	
 
-	if(count_flag){
-		query = query + " LIMIT " + limit;
-	}
+            //console.log(key + " -> " + object[key]);
+            //if (key != 'count'){
+            query = query + key + " LIKE ";
 
-	console.log("Final query is :"+query);
-	return query;
+            for(i = 0; i < object[key].length; i++){    		
+                query = query + object[key][i];
+                if(i != object[key].length - 1){
+                    query = query + " OR ";
+                }
+            }
+            //}
+
+        }
+    }
+
+    if(count_flag){
+        query = query + " LIMIT " + limit;
+    }
+
+    console.log("Final query is :"+query);
+    return query;
 }
 
 module.exports = {
-  objectToQuery: objectToQuery
+    objectToQuery: objectToQuery
 }
