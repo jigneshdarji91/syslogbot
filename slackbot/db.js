9a1a47437c0ef46981abc34178f73fe8dbde21d1
SaurabhSakpal <sfstoe@gmail.com>
2016-10-18 01:17:17 -0400

    db.js

--- slackbot/db.js
+++ slackbot/db.js
@@ -1,18 +1,29 @@
 var mysql      = require('mysql');
-var connection = mysql.createConnection({
+var logDB_connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
   password : 'sa123',
   database : 'slackBotLog_db'
 });
 
-connection.connect();
-
-connection.query('SELECT * from < table name >', function(err, rows, fields) {
-  if (!err)
-    console.log('The solution is: ', rows);
-  else
-    console.log('Error while performing Query.');
+var logDB_connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'sa123',
+  database : 'slackBotUser_db'
 });
 
-connection.end();
+
+function executeQuery (db_connection, query) {
+	db_connection.connect();
+	db_connection.query(query, function(err, rows, fields) {
+  		if (!err)
+    		console.log('The solution is: ', rows);
+  		else
+    		console.log('Error while performing Query.');
+	});
+	db_connection.end();
+}
+
+'SELECT server_ip, log_message from LogInfo'
+
