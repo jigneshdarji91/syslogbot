a5bff24c19592a17f974633e246fda2c019c6c42
Aparna Patil <aparnap754@gmail.com>
2016-10-20 15:28:25 -0400

    Working add and delete command

---
+++ slackbot/db.js
@@ -0,0 +1,69 @@
+var mysql = require('mysql');
+
+var logDB_connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'appu123',
+  database : 'slackBotLog_db'
+});
+
+var userDB_connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'appu123',
+  database : 'slackBotUser_db'
+});
+
+function getLogDBConnector() {
+	return logDB_connection;
+}
+
+function getUserDBConnector() {
+	return userDB_connection;
+}
+
+function executeLogQuery (query, resultHandler) {
+	var db_connection = getLogDBConnector();
+	db_connection.connect();
+	db_connection.query(query, function(err, rows, fields) {
+  		if (!err){
+    		var results = new Array(rows.length);
+    		var arrayLength = rows.length;
+    		for(var i = 0; i < arrayLength; i++){
+    			var result = new Array(2);
+    			result[0] = rows[i].server_ip;
+    			result[1] = rows[i].log_message;
+    			results[i] = result;
+    		}
+    		resultHandler(results);
+  		}
+  		else{
+  			console.log('Error Executing Query');
+    		resultHandler(null);
+  		}
+	});
+	db_connection.end();
+}
+
+function executeServerInfoQuery (query, resultHandler) {
+	var db_connection = getUserDBConnector();
+	db_connection.connect();
+	db_connection.query(query, function(err, rows, fields) {
+  		if (!err){
+    		resultHandler('Success');
+  		}
+  		else
+    		resultHandler('Error');
+	});
+	db_connection.end();
+}
+
+
+module.exports = {
+  getLogDBConnector: getLogDBConnector,
+  getUserDBConnector: getUserDBConnector,
+  executeLogQuery: executeLogQuery,
+  executeServerInfoQuery: executeServerInfoQuery
+}
+
+
---
+++ slackbot/queryGenerator.js
@@ -0,0 +1,57 @@
+/* * * * * * * * * * * * * * * * * * * *
+   Author: Prathamesh Ghanekar(pghanek)
+   Student ID: 200110766
+ * * * * * * * * * * * * * * * * * * * */
+
+/*this function generates a mySQL query from message object*/
+function objectToQuery(object) {
+    console.log("object: " + JSON.stringify(object));
+    var table_name = "logtable";// subject to change
+    var first_entry = 1;
+    var count_flag = 0;
+    var limit;
+
+    var query = "SELECT * FROM " + table_name + " WHERE ";
+    for (var key in object) {
+
+        if (object.hasOwnProperty(key)){
+            if(key == 'type')
+                continue;
+
+            if(key == 'count'){
+                count_flag = 1;
+                limit = object['count'];
+                continue;
+            }
+
+            if(!first_entry){
+                query = query + " AND ";
+            }
+            first_entry = 0;    
+
+            //console.log(key + " -> " + object[key]);
+            //if (key != 'count'){
+            query = query + key + " LIKE ";
+
+            for(i = 0; i < object[key].length; i++){            
+                query = query + object[key][i];
+                if(i != object[key].length - 1){
+                    query = query + " OR ";
+                }
+            }
+            //}
+
+        }
+    }
+
+    if(count_flag){
+        query = query + " LIMIT " + limit;
+    }
+
+    console.log("Final query is :"+query);
+    return query;
+}
+
+module.exports = {
+    objectToQuery: objectToQuery
+}
