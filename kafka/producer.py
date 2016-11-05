f1dee8d44c5fbbd1f43eb5a7e41207c1071e6220
Jignesh Darji <jndarji@ncsu.edu>
2016-11-05 16:49:13 -0400

    kafka: producer streams syslog messages

--- kafka/producer.py
+++ kafka/producer.py
@@ -1,4 +1,21 @@
 from kafka import KafkaProducer as Producer
+import time
+
 producer = Producer(bootstrap_servers='localhost:9092')
-for i in range(100):
-    producer.send('logs', 'this is a message: ' + str(i))
+
+def read_stream (filename):
+    syslogfile = open(filename,'r')
+    while True:
+        line = ''
+        while len(line) == 0 or line[-1] != '\n':
+            tail = syslogfile.readline()
+            if tail == '':
+                time.sleep(0.1)
+            line += tail
+        send_log (line)
+
+
+def send_log (message):
+    producer.send('logs', message)
+
+read_stream ('/var/log/system.log')
