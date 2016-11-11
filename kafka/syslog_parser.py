9cdb76d3df69842e4ef629d9318e37cd4dc3a55a
Jignesh Darji <jndarji@ncsu.edu>
2016-11-10 21:13:04 -0500

    log-parser: getting log level

--- kafka/syslog_parser.py
+++ kafka/syslog_parser.py
@@ -2,6 +2,7 @@ from pyparsing import Word, alphas, Suppress, Combine, nums, string, Optional, R
 from time import strftime
 import sys
 from datetime import datetime, date
+import re
 
 
 class Parser:
@@ -17,13 +18,22 @@ class Parser:
         payload = {}
         try:
             parsed = self.pattern.parseString(line)
-            payload["date"] = date.isoformat(datetime.strptime(parsed[0] + " 2016", "%b %d %Y"))
-            payload["time"] = parsed[1]
-            payload["host"] = parsed[2]
-            payload["app"] = parsed[3]
-            payload["content"] = parsed[4]
-            #print payload
+            payload["date"]         = date.isoformat(datetime.strptime(parsed[0] + " 2016", "%b %d %Y"))
+            payload["time"]         = parsed[1]
+            payload["host"]         = parsed[2]
+            payload["app"]          = parsed[3]
+            payload["content"]      = parsed[4]
+            payload["log_level"]    = Parser.get_log_level(payload["content"])
         except:
             print("error in parsing: ", sys.exc_info()[0])
 
         return payload
+
+    @staticmethod
+    def get_log_level(content):
+        if re.search('error', content, re.IGNORECASE) or re.search('err', content, re.IGNORECASE):
+            return 'ERROR'
+        elif re.search('warning', content, re.IGNORECASE) or re.search('warn', content, re.IGNORECASE):
+            return 'WARNING'
+        else:
+            return 'INFO'
