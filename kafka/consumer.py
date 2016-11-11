693a562c5c6724df3df47c781deb43e35bf7163b
Ubuntu <ubuntu@ip-172-31-19-171.us-west-2.compute.internal>
2016-11-11 00:20:20 +0000

    shifting message parsing code to the consumer

--- kafka/consumer.py
+++ kafka/consumer.py
@@ -1,8 +1,12 @@
 from kafka import KafkaConsumer as Consumer
+from syslog_parser import Parser
 import json
-
+parser = Parser()
 consumer = Consumer(bootstrap_servers='35.163.99.103:9092')
 consumer.subscribe(['logs'])
 #print (json.loads(consumer))
 for msg in consumer:
-    print msg
+    log_obj = parser.parse(msg.value)
+    log_obj["message"] = msg.value
+    print(log_obj)
+    #print msg
